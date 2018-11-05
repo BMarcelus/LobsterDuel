dc8b5c87cd4db69227d9e037da4fa8e0ee43ef45
BMarcelus <bmdizon@uci.edu>
2018-11-05 13:31:03 -0800

    In editor card changing

---
+++ RoyaltyFreeLobsterGame/Assets/Scripts/CardStats.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardStats : MonoBehaviour {
+  [Range(0, 3)]
+  public int level;
+  [Range(0, 3)]
+  public int attack;
+  [Range(0, 3)]
+  public int defense;
+
+  public PipsDisplay levelDisplay;
+  public PipsDisplay attackDisplay;
+  public PipsDisplay defenseDisplay;
+
+
+	// Use this for initialization
+	void OnValidate () {
+
+		levelDisplay.setLevel(level);
+		attackDisplay.setLevel(attack);
+		defenseDisplay.setLevel(defense);
+	}
+}
---
+++ RoyaltyFreeLobsterGame/Assets/Scripts/PipsDisplay.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipsDisplay : MonoBehaviour {
+
+  public GameObject[] pips;
+  private int level;
+
+  void updateDisplay() {
+    for(int i = 0; i < pips.Length; ++i) {
+      pips[i].SetActive(i<level);
+    }
+  }
+  public void setLevel(int l) {
+    level = l;
+    updateDisplay();
+  }
+}
