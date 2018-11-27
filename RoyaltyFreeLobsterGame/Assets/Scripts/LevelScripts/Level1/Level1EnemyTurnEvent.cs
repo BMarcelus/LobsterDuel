c91b05b699366ed07abc55e83d7b4c57d9bb5058
Weiyan Zhu <weiyanz@uci.edu>
2018-11-26 17:55:47 -0800

    Shitty Battle 1 cause I fail to follow the gameplay script

--- RoyaltyFreeLobsterGame/Assets/Scripts/LevelScripts/Level1/Level1EnemyTurnEvent.cs
+++ RoyaltyFreeLobsterGame/Assets/Scripts/LevelScripts/Level1/Level1EnemyTurnEvent.cs
@@ -3,20 +3,58 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Level1EnemyTurnEvent : EnemyTurnEvents {
+	public TurnManager turnManager;
+	public GameObject tutorialPage;
+	public EnemyHand enemyHand;
+	public Floor enemyFloor;
+	public BattleManager battleManager;
+	public GameObject dialogue;
+
+	void Start()
+	{
+		dialogue.SetActive(false);
+	}
 
 	public override IEnumerator CheckTurnEvent(int turn)
 	{
+		//tutorialPage.SetActive(false);
 		switch(turn)
 		{
 			case 1:
-			yield return Turn1Event();
+				yield return Turn1Event();
+				break;
+			case 6:
+				yield return Turn6Event();
+				break;
+			default:
+				enemyManager.StartEnemyTurn();
 				break;
 		}
 	}
 
 	public IEnumerator Turn1Event()
 	{
-		yield return new WaitForSeconds(1);
+		//draw card
+		enemyHand.AddCardToHand();
+		dialogue.SetActive(true);
+		yield return new WaitForSeconds(1.5f);
+		dialogue.SetActive(false);
+		enemyManager.StartEnemyTurn();
+		//turnManager.SwitchToPlayer();
+	}
+
+	public IEnumerator Turn6Event()
+	{
+		//fill the spot with rocks
+		foreach(GameObject spot in enemyFloor.spots)
+		{
+			if(!spot.GetComponent<FloorSpot>().GetCardInPlay())
+			{
+				spot.GetComponent<FloorSpot>().SetCardWithData(battleManager.rockData);
+			}
+		}
+		yield return new WaitForSeconds(1.5f);
+		enemyManager.StartEnemyTurn();
 	}
 
 }
