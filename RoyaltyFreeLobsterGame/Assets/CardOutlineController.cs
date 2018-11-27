1db048930e1bfdff890230f3e6f784f12c0875d6
BMarcelus <bmdizon@uci.edu>
2018-11-26 21:18:52 -0800

    mouseover effects and more assets updates

---
+++ RoyaltyFreeLobsterGame/Assets/CardOutlineController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardOutlineController : MonoBehaviour {
+
+  public Color canAttackColor;
+  public Color canPlaceColor;
+  public Color disabledColor;
+  private Outline outline;
+  private Lobster lobster;
+  private PlayerHand hand;
+	// Use this for initialization
+	void Start () {
+		outline = GetComponentInChildren(typeof(Outline), false) as Outline;
+    lobster = GetComponent<Lobster>();
+    hand = GetComponentInParent(typeof(PlayerHand)) as PlayerHand;
+    if(!hand && (lobster && lobster.floorAssigned && lobster.floorAssigned.tag != "PlayerSpot")) {
+      this.enabled = false;
+    }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(lobster.enabled && lobster.canAttack) {
+      outline.effectColor = Color.Lerp(outline.effectColor, canAttackColor, 0.1f);
+    } else if(!lobster.enabled && GetCanPlaceCard()) {
+      outline.effectColor = Color.Lerp(outline.effectColor, canPlaceColor, 0.1f);
+    } else {
+      outline.effectColor = Color.Lerp(outline.effectColor, disabledColor, 0.5f);
+    }
+	}
+
+  private bool GetCanPlaceCard() {
+    if(hand) return hand.canPlaceCard;
+    return false;
+  }
+
+}
---
+++ RoyaltyFreeLobsterGame/Assets/SizeOnMouseOver.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SizeOnMouseOver : MonoBehaviour {
+
+  private Vector3 size;
+  public GameObject graphic;
+  private bool highlighted = false;
+  private SpriteRenderer[] spriteRenderers;
+  private Canvas[] canvases;
+  public bool active = true;
+	// Use this for initialization
+	void Start () {
+		size = graphic.transform.localScale;
+    spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+    canvases = GetComponentsInChildren<Canvas>();
+    if(!GetComponentInParent<PlayerHand>()) active = false;
+	}
+
+  void Update() {
+    if(GetComponent<Lobster>().enabled) {
+      OnMouseExit();
+      active = false;
+    }
+  }
+  
+  void OnMouseOver() {
+    if(!active)return;
+    graphic.transform.localScale = size*1.5f;
+    graphic.transform.localPosition = Vector3.up*size.y*.8f;
+    if(!highlighted) {
+      highlighted = true;
+      foreach(SpriteRenderer spriteRenderer in spriteRenderers) {
+        spriteRenderer.sortingOrder += 10;
+      }
+      foreach(Canvas canvas in canvases) {
+        canvas.sortingOrder += 10;
+      }
+    }
+  }
+
+  void OnMouseExit() {   
+    if(!active)return;
+    graphic.transform.localScale = size;
+    graphic.transform.localPosition = Vector3.zero;
+    if(highlighted) {
+      highlighted = false;
+      foreach(SpriteRenderer spriteRenderer in spriteRenderers) {
+        spriteRenderer.sortingOrder -= 10;
+      }
+      foreach(Canvas canvas in canvases) {
+        canvas.sortingOrder -= 10;
+      }
+    }
+  }
+}
