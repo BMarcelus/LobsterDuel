fbbf9f3b32894fbb56499e51091f21bfb0177ef6
BMarcelus <bmdizon@uci.edu>
2018-11-29 15:22:04 -0800

    stuff

---
+++ RoyaltyFreeLobsterGame/Assets/CardDefenseBlockDisplay.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardDefenseBlockDisplay : MonoBehaviour {
+
+  private SpriteRenderer[] spriteRenderers;
+  private Canvas[] canvases;
+  private GameObject defenseBlockDisplay;
+  private bool defending = false;
+	// Use this for initialization
+	void Start () {
+    spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+    canvases = GetComponentsInChildren<Canvas>();
+    if(transform.parent.name == "playerHand") {
+      defenseBlockDisplay = GameObject.Find("DefenseBlockDisplayPlayer");
+    } else {
+      defenseBlockDisplay = GameObject.Find("DefenseBlockDisplayEnemy");
+    }
+	}
+	
+	public void OnDefenseStart () {
+    return;
+		if(defending) return;
+    defending = true;
+    if(defenseBlockDisplay) {
+      defenseBlockDisplay.GetComponent<Animator>().Play("DefenseBlock", -1, 0);
+    } else {
+      Debug.Log("HELP");
+    }
+    foreach(SpriteRenderer spriteRenderer in spriteRenderers) {
+      spriteRenderer.sortingOrder += 20;
+    }
+    foreach(Canvas canvas in canvases) {
+      canvas.sortingOrder += 20;
+    }
+	}
+
+  public void OnDefenseEnd() {
+    return;
+		if(!defending) return;
+    defending = false;
+    foreach(SpriteRenderer spriteRenderer in spriteRenderers) {
+      spriteRenderer.sortingOrder -= 20;
+    }
+    foreach(Canvas canvas in canvases) {
+      canvas.sortingOrder -= 20;
+    }
+  }
+}
